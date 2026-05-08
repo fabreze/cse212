3ce7dc748db31eb584b8492be0214be40d5c16f0
Fabringio <fcaballero.pbl@gmail.com>
2026-05-08 17:00:01 -0300

    W01 Code: Dynamic Arrays

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,3 +1,5 @@
+using System.Data;
+
 public static class Arrays
 {
     /// <summary>
@@ -13,7 +15,15 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        List<double> dynamicArray = new(); // Create an empty dynamic array
+        for (int i = 1; i <= length; i++) // Starting at 1, multiply i by the number to get the multiples of it and add it to the list
+        {
+            double multiple = number * i;
+            dynamicArray.Add(multiple);
+        }
+
+        double[] results = dynamicArray.ToArray();//convert the list to a fixed array
+        return results;
     }
 
     /// <summary>
@@ -29,5 +39,30 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        //Create an new empty list
+        List<int> results = new();
+
+        //Check if the amount is equal to the size of the list or zero
+        if (data.Count == amount || amount == 0)
+        {
+            return; //Stop execution as no changes need to be made
+        }
+        else
+        {
+            //Create an integer that is the index from where the list will be sliced
+            int index = data.Count - amount;
+            //Create an array from the first half of the list
+            List<int> firstHalf = data.GetRange(0, index);
+            //Create an array from the second half of the list
+            List<int> secondHalf = data.GetRange(index, amount);
+
+            //Clear the data list
+            data.Clear();
+
+            //Add both ranges
+            data.AddRange(secondHalf);
+            data.AddRange(firstHalf);
+        }
     }
 }
