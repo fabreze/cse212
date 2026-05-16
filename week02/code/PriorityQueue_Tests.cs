6eb4304d308f27f4b6df5c33a86c8865ee95c7d7
fabreze <fcaballerourquhart@byupathway.edu>
2026-05-16 18:26:54 -0300

    W02 Code: Queues

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -1,3 +1,4 @@
+using System.IO.Pipelines;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 // TODO Problem 2 - Write and run test cases and fix the code to match requirements.
@@ -6,24 +7,45 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
+    // Scenario: Happy path priority queue
+    // Expected Result: ["Tim","Sue","Bob"]
     // Defect(s) Found: 
     public void TestPriorityQueue_1()
     {
+        String[] expectedResult =["Tim","Sue","Bob"];
+
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue("Bob", 2);
+        priorityQueue.Enqueue("Tim", 5);
+        priorityQueue.Enqueue("Sue", 3);
+
+
+        for(int i = 0; i < expectedResult.Length; i++)
+        {
+            string result = priorityQueue.Dequeue();
+            Assert.AreEqual(expectedResult[i], result);
+        }
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
+    // Scenario: Shared priority queue
+    // Expected Result: ["Tim","Jack","Sue","Bob"]
     // Defect(s) Found: 
     public void TestPriorityQueue_2()
     {
-        var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
-    }
+        String[] expectedResult =["Tim","Jack","Sue","Bob"];
 
-    // Add more test cases as needed below.
+        var priorityQueue = new PriorityQueue();
+        priorityQueue.Enqueue("Bob", 2);
+        priorityQueue.Enqueue("Tim", 5);
+        priorityQueue.Enqueue("Sue", 3);
+        priorityQueue.Enqueue("Jack", 5);
+
+
+        for(int i = 0; i < expectedResult.Length; i++)
+        {
+            string result = priorityQueue.Dequeue();
+            Assert.AreEqual(expectedResult[i], result);
+        }
+    }
 }
