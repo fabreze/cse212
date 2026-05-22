cd4a8eeed345e20fbf7c3bbd8723ef739bbb963c
Fabringio <fcaballero.pbl@gmail.com>
2026-05-22 17:00:59 -0300

    W02 Code: Queues Corrections

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -9,10 +9,10 @@ public class PriorityQueueTests
     [TestMethod]
     // Scenario: Happy path priority queue
     // Expected Result: ["Tim","Sue","Bob"]
-    // Defect(s) Found: 
+    // Defect(s) Found: Dequeue does not remove the item and returns the same item repeatedly. Additionally the loop skips the last item of the list
     public void TestPriorityQueue_1()
     {
-        String[] expectedResult =["Tim","Sue","Bob"];
+        String[] expectedResult = ["Tim", "Sue", "Bob"];
 
         var priorityQueue = new PriorityQueue();
         priorityQueue.Enqueue("Bob", 2);
@@ -20,7 +20,7 @@ public class PriorityQueueTests
         priorityQueue.Enqueue("Sue", 3);
 
 
-        for(int i = 0; i < expectedResult.Length; i++)
+        for (int i = 0; i < expectedResult.Length; i++)
         {
             string result = priorityQueue.Dequeue();
             Assert.AreEqual(expectedResult[i], result);
@@ -30,10 +30,10 @@ public class PriorityQueueTests
     [TestMethod]
     // Scenario: Shared priority queue
     // Expected Result: ["Tim","Jack","Sue","Bob"]
-    // Defect(s) Found: 
+    // Defect(s) Found: >= when finding the highest priority item to remove causes last-in, first out behavior
     public void TestPriorityQueue_2()
     {
-        String[] expectedResult =["Tim","Jack","Sue","Bob"];
+        String[] expectedResult = ["Tim", "Jack", "Sue", "Bob"];
 
         var priorityQueue = new PriorityQueue();
         priorityQueue.Enqueue("Bob", 2);
@@ -42,10 +42,39 @@ public class PriorityQueueTests
         priorityQueue.Enqueue("Jack", 5);
 
 
-        for(int i = 0; i < expectedResult.Length; i++)
+        for (int i = 0; i < expectedResult.Length; i++)
         {
             string result = priorityQueue.Dequeue();
             Assert.AreEqual(expectedResult[i], result);
+        }
+    }
+
+    [TestMethod]
+    // Scenario: Shared priority queue
+    // Expected Result: ["Tim","Jack","Sue","Bob"]
+    // Defect(s) Found: None
+    public void TestPriorityQueue_3()
+    {
+        var priorityQueue = new PriorityQueue();
+        try
+        {
+            priorityQueue.Dequeue();
+            Assert.Fail("Exception should have been thrown.");
+        }
+        catch (InvalidOperationException e)
+        {
+            Assert.AreEqual("The queue is empty.", e.Message);
+        }
+        catch (AssertFailedException)
+        {
+            throw;
+        }
+        catch (Exception e)
+        {
+            Assert.Fail(
+                 string.Format("Unexpected exception of type {0} caught: {1}",
+                                e.GetType(), e.Message)
+            );
         }
     }
 }
